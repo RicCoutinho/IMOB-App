c17b6da629c7c0caadca50645f3a29b2b800ef05
Ricardo Coutinho <dinhofonte@hotmail.com>
2019-05-24 19:50:52 -0300

    Telas
    Criação de Telas

---
+++ src/mouseManipulator/MouseManipulator.java
@@ -0,0 +1,25 @@
+/*import view.TelaLogin;
+
+jTextFieldUser campo_entrada = new JTextFieldUser(40);
+
+ManipuladorMouse manipula_mouse = new ManipuladorMouse();
+
+} 
+...
+ private class ManipuladorMouse implements MouseListener{
+        public void mousePressed(MouseEvent e){
+                campo_entrada.setText("");
+        }
+        
+         public void mouseEntered(MouseEvent e){
+            }
+
+         public void mouseExited(MouseEvent e){
+            }
+
+         public void mouseClicked(MouseEvent e){
+            }
+
+         public void mouseReleased(MouseEvent e){
+            }
+    }*/
---
+++ src/view/TelaPrincipal.java
@@ -0,0 +1,149 @@
+package view;
+
+import Utilities.FundoTela;
+import java.awt.GridLayout;
+
+public class TelaPrincipal extends javax.swing.JFrame {
+
+    FundoTela tela;
+    
+    
+    public TelaPrincipal() {
+        initComponents();
+        setExtendedState(MAXIMIZED_BOTH);
+        setLayout(new GridLayout());
+        tela = new FundoTela("Imagens/backGround.jpg");
+        getContentPane().add(tela);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Imob");
+
+        jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/user-shape.png"))); // NOI18N
+        jMenu1.setText("Conta");
+
+        jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/user (1).png"))); // NOI18N
+        jMenuItem1.setText("Perfil");
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Desconectar");
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/home.png"))); // NOI18N
+        jMenu2.setText("Cliente");
+
+        jMenuItem3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/plus.png"))); // NOI18N
+        jMenuItem3.setText("Cadastrar");
+        jMenu2.add(jMenuItem3);
+
+        jMenuItem4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/magnifier.png"))); // NOI18N
+        jMenuItem4.setText("Pesquisar");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem4);
+
+        jMenuItem5.setText("jMenuItem5");
+        jMenu2.add(jMenuItem5);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icon.png"))); // NOI18N
+        jMenu4.setText("Sobre");
+        jMenu4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu4MouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jMenu4MouseEntered(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jMenu4MousePressed(evt);
+            }
+        });
+        jMenu4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenu4ActionPerformed(evt);
+            }
+        });
+        jMenuBar1.add(jMenu4);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 725, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 443, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+       
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenu4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu4ActionPerformed
+
+    }//GEN-LAST:event_jMenu4ActionPerformed
+
+    private void jMenu4MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu4MousePressed
+        TelaSobre Ts = new TelaSobre();
+        Ts.setVisible(true);
+    }//GEN-LAST:event_jMenu4MousePressed
+
+    private void jMenu4MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu4MouseEntered
+
+    }//GEN-LAST:event_jMenu4MouseEntered
+
+    private void jMenu4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu4MouseClicked
+
+    }//GEN-LAST:event_jMenu4MouseClicked
+
+  
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaPrincipal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    // End of variables declaration//GEN-END:variables
+}
